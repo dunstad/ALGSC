13ad6622eef11a694b8d8fb64409e192c550b420
dunstad <anthony00.blount00@gmail.com>
2020-10-19 09:25:02 +0900

    organization

--- client/main-menu.js
+++ client/main-menu.js
@@ -4,6 +4,39 @@ var blessed = require('neo-blessed');
 // Create a screen object.
 var screen = blessed.screen();
 
+var image = blessed.image({
+  file: './city.png',
+  top: 'center',
+  left: 'center',
+  height: Math.min(screen.height, 40),
+  width: Math.min(screen.width, 83 * 2),
+  // height: 13,
+  // width: 80,
+  tags: true,
+  border: {
+    type: 'line'
+  },
+  style: {
+    fg: 'white',
+    bg: 'magenta',
+    border: {
+      fg: '#ffffff'
+    },
+    hover: {
+      bg: 'green'
+    }
+  }
+});
+
+// Append our image to the screen.
+screen.append(image);
+
+// Quit on Escape, q, or Control-C.
+screen.key(['escape', 'q', 'C-c'], function(ch, key) {
+  return process.exit(0);
+});
+
+
 let mainMenuItems = [
   {
     name: 'Single Player',
@@ -49,38 +82,7 @@ let mainMenu = blessed.list({
 
 mainMenu.on('select', (item, index)=>{mainMenuItems[index].action();});
 
-var image = blessed.image({
-  file: './city.png',
-  top: 'center',
-  left: 'center',
-  height: Math.min(screen.height, 40),
-  width: Math.min(screen.width, 83 * 2),
-  // height: 13,
-  // width: 80,
-  tags: true,
-  border: {
-    type: 'line'
-  },
-  style: {
-    fg: 'white',
-    bg: 'magenta',
-    border: {
-      fg: '#ffffff'
-    },
-    hover: {
-      bg: 'green'
-    }
-  }
-});
-
-// Append our image to the screen.
-screen.append(image);
 screen.append(mainMenu);
-
-// Quit on Escape, q, or Control-C.
-screen.key(['escape', 'q', 'C-c'], function(ch, key) {
-  return process.exit(0);
-});
 
 // Focus our element.
 mainMenu.focus();
